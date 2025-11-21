fd3630061a7be261412eb17e9ca0de86a1372411
aju1221 <ajmalmk707@gmail.com>
2025-11-21 11:22:38 +0530

    Create index.js

---
+++ index.js
@@ -0,0 +1,6 @@
+import React from "react";
+import { createRoot } from "react-dom/client";
+import CeecoChatbot from "./CeecoChatbot";
+
+const root = createRoot(document.getElementById("root"));
+root.render(<CeecoChatbot />);
